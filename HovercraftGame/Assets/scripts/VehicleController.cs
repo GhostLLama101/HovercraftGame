cfba5c748012a306549245d0ff140366366c1712
Joseph Dadlez <jsdadlez@outlook.com>
2026-04-04 15:12:50 -0700

    Added checkpoints
    The check points work and they change colors and highlight based on the next one

--- HovercraftGame/Assets/scripts/VehicleController.cs
+++ HovercraftGame/Assets/scripts/VehicleController.cs
@@ -4,29 +4,47 @@ using UnityEngine.InputSystem;
 public class VehicleController : MonoBehaviour
 {
 
-    public float desired_acceleration = 1.0f;
+    public float desired_acceleration_x = 0f;
+    
+    public float desired_acceleration_z = 0f;
+
+
+    public float impulse;
+
+    public float turnrate;
+
+    public CheckpointController target;
+
+    public Rigidbody _rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+        target.left.materials[0].color = Color.red;
+        target.right.materials[0].color = Color.red;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Rigidbody>().AddRelativeForce(desired_acceleration*10, 0, 0);
+        _rb.AddRelativeForce(desired_acceleration_x*impulse, 0, desired_acceleration_z*impulse);
         
-        float dx = (Mouse.current.position.x.value - Screen.width / 2) / 200;
+        float dx = (Mouse.current.position.x.value - Screen.width / 2) / turnrate;
         if (Mathf.Abs(dx) > 0.01f)
         {
             transform.Rotate(0, dx, 0);
         }
+        
+        
     }
     
     void OnMove(InputValue action)
     {
         var movement = action.Get<Vector2>();
-        desired_acceleration = movement.y;
-        // add the horizontal input in z direction
+        desired_acceleration_x = movement.y; // move in the x direction
+        desired_acceleration_z = -movement.x; // move in  the z direction
+        
+        Debug.Log(movement);
+        
     }
 }
